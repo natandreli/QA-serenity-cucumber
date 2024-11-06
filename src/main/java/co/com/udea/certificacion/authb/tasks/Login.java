6374d3b8f4fb85a6c7ea231ffcf6db90c9ba5a6b
natandreli <natalia.garcia9@udea.edu.co>
2024-11-06 16:00:04 -0500

    Refactor login tests

---
+++ src/main/java/co/com/udea/certificacion/authb/tasks/Login.java
@@ -0,0 +1,45 @@
+package co.com.udea.certificacion.authb.tasks;
+
+import io.restassured.http.ContentType;
+import net.serenitybdd.rest.SerenityRest;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.rest.interactions.Post;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+import java.util.Map;
+
+public class LoginAsUser implements Task {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginAsUser.class);
+    private final Map<String, String> credentials;
+    private String authToken;
+
+    public LoginAsUser(Map<String, String> credentials) {
+        this.credentials = credentials;
+    }
+
+    public static LoginAsUser withCredentials(Map<String, String> credentials) {
+        return instrumented(LoginAsUser.class, credentials);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Post.to("/users/v1/login")
+                        .with(request -> request
+                                .contentType(ContentType.JSON)
+                                .body(credentials)
+                                .relaxedHTTPSValidation()
+                        )
+        );
+
+        authToken = SerenityRest.lastResponse().jsonPath().getString("auth_token");
+        LOGGER.info("User Auth Token: {}", authToken);
+
+        // Guardar el token en el contexto de Serenity para uso posterior
+        actor.remember("authToken", authToken);
+    }
+}
